4031149a114d06d676bef2c8eedbfcddc56090c0
Alex <zemzikova.oleksandra@gmail.com>
2025-03-26 15:30:03 +0200

    Fix GitHub Pages Deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  target: "static",
+
+  app: {
+    baseURL: "/test-penman/",
+  },
+
+  compatibilityDate: "2025-03-26",
+});
