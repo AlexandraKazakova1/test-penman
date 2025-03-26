fea4adf18474a8ca4187f16616daf0283aba4797
Alex <zemzikova.oleksandra@gmail.com>
2025-03-26 16:09:38 +0200

    fr

---
+++ server/api/flight_data.js
@@ -0,0 +1,14 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+export default defineEventHandler(() => {
+  const filePath = join(process.cwd(), "server", "data", "flight_data.json");
+
+  try {
+    const fileContent = readFileSync(filePath, "utf-8");
+    return JSON.parse(fileContent);
+  } catch (error) {
+    console.error("Ошибка при чтении flight_data.json:", error);
+    return { error: "Не удалось загрузить данные полета" };
+  }
+});
